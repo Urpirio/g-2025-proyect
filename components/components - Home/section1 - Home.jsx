3041024a1edff0163c8a0abeb841d7e8b2ee096e
UrpirioDev <133790157+Urpirio@users.noreply.github.com>
2025-06-18 19:02:08 -0400

    Se hicieron mejoras al estilo

--- components/components - Home/section1 - Home.jsx
+++ components/components - Home/section1 - Home.jsx
@@ -9,10 +9,10 @@ export default function Section1Home() {
         md:mask-l-to-60% 
         mask-radial-at-center 
         lg:mask-l-from-30% 
-        lg:mask-l-to-60%  w-dvw lg:h-dvh hidden md:flex justify-end items-start">
+        lg:mask-l-to-60%  w-dvw lg:h-dvh hidden md:flex justify-end items-start z-20">
             <Image className="   object-contain" src={imgFamily} alt=""/>
         </div>
-        <div className=" md:absolute boder w-full md:py-20 lg:py-20 lg:h-[80%] flex flex-col justify-center gap-5 px-10 ">
+        <div className=" md:absolute z-20 boder w-full md:py-20 lg:py-20 lg:h-[80%] flex flex-col justify-center gap-5 px-10 ">
             <div className="w-[100%] text-center md:text-start md:w-[65%] lg:w-[45%]">
                 <h1 className="text-5xl font-semibold">
                     <span className="text-fblue font-black">Conectamos</span> a los ciudadanos con sus <span className="text-fblue font-black">beneficios</span>, de forma rápida, digna y transparente.
