297e20c618e07f559e3ad280af7ee8bc66af99d7
UrpirioDev <133790157+Urpirio@users.noreply.github.com>
2025-06-18 16:26:52 -0400

    XD

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -4,10 +4,15 @@ import Section1Home from "../../components/components - Home/section1 - Home";
 import Section2Home from "../../components/components - Home/section2 - Home";
 import Section3Home from "../../components/components - Home/section3 - Home";
 import Section4Home from "../../components/components - Home/section4 - Home";
+import { Geist } from 'next/font/google'
+
+const poppins = Geist({
+  subsets: ['latin'],
+});
 
 export default function page() {
   return (
-    <body>
+    <body className={`${poppins.className}`}>
       <Header/>
         <main>
           <Section1Home/>
