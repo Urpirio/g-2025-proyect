21a0402bd223fbc44d40445b902220d59c547c72
UrpirioDev <133790157+Urpirio@users.noreply.github.com>
2025-06-18 19:02:27 -0400

    Se concluyo la segunda section

--- components/components - Home/section2 - Home.jsx
+++ components/components - Home/section2 - Home.jsx
@@ -1,13 +1,18 @@
 import Image from "next/image";
+import ImgSection2 from '@/../public/img/img - Home/Group 135.png';
 
 export default function Section2Home() {
   return (
-    <section>
-        <div>
-            <Image src={''} alt=""/>
+    <section className="flex flex-wrap py-20">
+        <div className=" h-[100%] w-[100%] px-10 md:w-[50%] min-w-80 flex justify-end">
+            <Image className="object-cover" src={ImgSection2} alt="ImgSection"/>
         </div>
-        <div>
-            <span></span>
+        <div className=" w-[100%] md:w-[50%] min-w-80 flex justify-end  p-10">
+            <span className="sm:text-xl lg:text-5xl font-semibold  w-[100%]">
+                <span className="text-fblue">Plataforma gubernamental</span> que permite a los ciudadanos consultar,
+                 solicitar bonos y ayudas sociales. De manera transparente, 
+                 facil y eficiente.
+            </span>
         </div>
     </section>
   )
