6dfa34f3b260d09dbb4b16aa993f2e7727598dd3
UrpirioDev <133790157+Urpirio@users.noreply.github.com>
2025-06-18 16:13:43 -0400

    Bases de la section 3

---
+++ components/components - Home/section3 - Home.jsx
@@ -0,0 +1,25 @@
+import Image from "next/image";
+
+export default function Section3Home() {
+  return (
+   <section>
+    <div>
+        {/* <h1>¿Por qué elegirnos?</h1> */}
+    </div>
+    <div>
+        <div>
+            <Image src={''} alt=""/>
+        </div>
+        <div>
+
+        </div>
+        <div>
+
+        </div>
+        <div>
+
+        </div>
+    </div>
+   </section>
+  )
+}
