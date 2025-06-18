e4d4e40f16852fdc4400902edf303fb123fdcb55
UrpirioDev <133790157+Urpirio@users.noreply.github.com>
2025-06-18 16:13:33 -0400

    bases de las section 2

---
+++ components/components - Home/section2 - Home.jsx
@@ -0,0 +1,14 @@
+import Image from "next/image";
+
+export default function Section2Home() {
+  return (
+    <section>
+        <div>
+            <Image src={''} alt=""/>
+        </div>
+        <div>
+            <span></span>
+        </div>
+    </section>
+  )
+}
