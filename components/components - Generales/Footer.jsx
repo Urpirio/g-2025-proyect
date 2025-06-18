e67354ab5793c301fe21deae11ceb851be09f6d9
UrpirioDev <133790157+Urpirio@users.noreply.github.com>
2025-06-18 16:13:04 -0400

    Archivo del footer

---
+++ components/components - Generales/Footer.jsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Footer() {
+  return (
+    <footer>
+        {/* Esto Se trabajara en la ultima fase ya que lo siento incompleto */}
+    </footer>
+  )
+}
