caa2dfd58253f7c2b9d13d4febf5660c86299368
UrpirioDev <133790157+Urpirio@users.noreply.github.com>
2025-06-18 16:15:46 -0400

    Se agregaron las tarjetas de la section 4

---
+++ SubComponents/Subcomponents - Home/CardSection4.jsx
@@ -0,0 +1,14 @@
+
+export default function CardSection4() {
+  return (
+    <article>
+        <header>
+            <Image src={ ''} alt=""/>
+        </header>
+        <main>
+            <h1>Solicitud de bono</h1>
+            <p>Aplica directamente a los pogramas sociales disponibles.</p>
+        </main>
+    </article>
+  )
+}
