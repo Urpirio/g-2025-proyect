e9ebf4d746394130c42e5e4fb71149ad9a18eb41
UrpirioDev <133790157+Urpirio@users.noreply.github.com>
2025-06-18 16:13:48 -0400

    Bases de la section 4

---
+++ components/components - Home/section4 - Home.jsx
@@ -0,0 +1,13 @@
+
+export default function Section4Home() {
+  return (
+    <section>
+        <div>
+            {/* <h1>Servicios que de adaptan a tus necesidades</h1> */}
+        </div>
+        <div>
+
+        </div>
+    </section>
+  )
+}
