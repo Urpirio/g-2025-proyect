25c8a694009bbd054f88967d859e8b7a0fd3dc54
UrpirioDev <133790157+Urpirio@users.noreply.github.com>
2025-06-18 16:15:31 -0400

    Se agrego las bases para las tarjetas de la section 3

---
+++ SubComponents/Subcomponents - Home/CardSection3.jsx
@@ -0,0 +1,15 @@
+import Image from "next/image";
+import { HiMiniGiftTop } from "react-icons/hi2";
+
+export default function CardSection3() {
+  return (
+    <article>
+        <div>
+            <Image src={''} alt=""/>
+        </div>
+        <div>
+            <h1>Facilita la planificación y monitoreo social</h1>
+        </div>
+    </article>
+  )
+}
