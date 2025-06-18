cf2db5b1a362aa09da2e5393c45f4196e8430696
UrpirioDev <133790157+Urpirio@users.noreply.github.com>
2025-06-18 16:14:57 -0400

    Se agregaron las section del home

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,9 +1,19 @@
 
+import Header from "../../components/components - Generales/Header";
+import Section1Home from "../../components/components - Home/section1 - Home";
+import Section2Home from "../../components/components - Home/section2 - Home";
+import Section3Home from "../../components/components - Home/section3 - Home";
+import Section4Home from "../../components/components - Home/section4 - Home";
+
 export default function page() {
   return (
     <body>
+      <Header/>
         <main>
-
+          <Section1Home/>
+          <Section2Home/>
+          <Section3Home/>
+          <Section4Home/>
         </main>
     </body>
   )
