30cd5b9c09ea93fe04c95011b3d741e5bd8ad66a
UrpirioDev <133790157+Urpirio@users.noreply.github.com>
2025-06-23 11:50:45 -0400

    Se agrego las pagina de servicio

---
+++ src/app/Servicios/page.jsx
@@ -0,0 +1,12 @@
+import Header from "../../../components/components - Generales/Header";
+
+export default function page() {
+  return (
+    <body>
+        <Header/>
+        <main>
+            
+        </main>
+    </body>
+  )
+}
