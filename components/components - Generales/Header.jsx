107b7320e45304129cdb1c1351c585f15fcc38bd
UrpirioDev <133790157+Urpirio@users.noreply.github.com>
2025-06-23 11:48:30 -0400

    Se agrego el menu de navegacion movile

--- components/components - Generales/Header.jsx
+++ components/components - Generales/Header.jsx
@@ -1,10 +1,21 @@
+'use client';
 import Image from "next/image";
 import Link from "next/link";
-import Logo from '@/../public/img/img - Home/Rectangle 101.png'
+import Logo from '@/../public/img/img - Home/Rectangle 101.png';
+import { IoMenu } from "react-icons/io5";
+import { IoMdClose } from "react-icons/io";
+import { useState } from "react";
+import { usePathname } from "next/navigation";
 
+export let SetDeployMenu;
 export default function Header() {
+    
+  const Pathname = usePathname();
+  const [DeployMenu,setDeployMenu] = useState(false);
+  SetDeployMenu = setDeployMenu;
+
   return (
-    <header className="flex border border-gray-300 shadow-2xs justify-between items-center sticky top-0 z-50 bg-white py-4 px-5">
+    <header className="flex border border-gray-300 shadow-2xs justify-between  sticky top-0 z-50 bg-white py-4 px-5 w-full">
         <div className="flex  w-40 lg:w-p3   justify-start"> 
             <Image className="h-10 w-50 object-contain" src={Logo} alt=""/>
         </div>
@@ -16,9 +27,25 @@ export default function Header() {
             </nav>
         </div>
         <div className="  lg:w-p3 hidden md:flex justify-end  items-center gap-1">
-            <button className="border px-5 py-2 rounded-xl bg-fblue text-white">Iniciar Sesión</button>
-            <button className="border px-5 py-2 rounded-xl bg-gray-400 text-white">Registrarse</button>
+            <Link href={''} className="border px-5 py-2 rounded-xl bg-fblue text-white">Iniciar Sesión</Link>
+            <Link href={''} className="border px-5 py-2 rounded-xl bg-gray-400 text-white">Registrarse</Link>
         </div>
+        <div className=" md:hidden flex items-center">
+            <button className="text-4xl" onClick={()=>{setDeployMenu(!DeployMenu)}}>
+                {DeployMenu ? <IoMdClose className="text-red-600"/>: <IoMenu className="text-blue-950"/>}
+            </button>
+        </div>
+        <menu className={`${DeployMenu ? 'flex' : 'hidden'} flex-col p-5 border-b border-gray-300 rounded-b-2xl shadow bg-white  absolute right-0 w-full top-18`}>
+            <div className="flex flex-col gap-2 ">
+                <Link href={''} className={`${Pathname === '/' ? 'text-blue-600' : ''}`}>Inicio</Link>
+                <Link href={''}>Sobre Nosotros</Link>
+                <Link href={''}>Servicios</Link>
+            </div>
+            <div className="flex flex-col gap-2 py-2">
+                <Link href={''} className="text-center border px-5 py-2 w-full rounded-xl bg-fblue text-white">Iniciar Sesión</Link>
+                <Link href={''} className="text-center border px-5 py-2 w-full rounded-xl bg-gray-400 text-white">Regristrarse</Link>
+            </div>
+        </menu>
     </header>
   )
 }
