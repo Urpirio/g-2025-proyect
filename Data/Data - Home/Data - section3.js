4c9178cfec975a0e067ced11be917a1228a2bab6
UrpirioDev <133790157+Urpirio@users.noreply.github.com>
2025-06-23 11:49:54 -0400

    Se agrego el array de datos de la section 3 esto para las cartas

---
+++ Data/Data - Home/Data - section3.js
@@ -0,0 +1,40 @@
+import IconAnalisis from '@/../public/Icon/Icon - Home/analytics_9647958 1.png';
+import Iconprotected from '@/../public/Icon/Icon - Home/protected.png';
+import Iconbudget from '@/../public/Icon/Icon - Home/budget.png';
+import Iconatm_card from '@/../public/Icon/Icon - Home/atm-card.png';
+import Icongraph from '@/../public/Icon/Icon - Home/graph.png';
+import Iconinfinity from '@/../public/Icon/Icon - Home/infinity.png';
+
+export const DataSection3 = [
+    {
+        key:1,
+        Icon: IconAnalisis,
+        Titulo: 'Facilita la planificación y monitoreo social',
+    },
+    {
+        key:2,
+        Icon: Iconprotected,
+        Titulo: 'Transparencia y trazabilidad',
+    },
+    {
+        key:3,
+        Icon: Iconbudget,
+        Titulo: 'Reducción de costos',
+    },
+    {
+        key:4,
+        Icon: Iconatm_card,
+        Titulo: 'Evita duplicaciones',
+    },
+    {
+        key:5,
+        Icon: Iconinfinity,
+        Titulo: 'Unificación',
+    },
+    {
+        key:6,
+        Icon: Icongraph,
+        Titulo: 'Mejora la experiencia del ciudadano',
+    },
+    
+]
